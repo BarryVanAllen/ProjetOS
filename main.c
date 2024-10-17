ab2c0b83d2062c34ac14cb5c99228b42c0b04314
BarryVanAllen <theo6740@hotmail.fr>
2024-10-17 12:14:43 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+int secteur(int *voiture);
+
+int main()
+{
+    int voiture = 11;
+
+    secteur(voiture);
+}
+
+int secteur(int *voiture)
+{
+    srand(time (0));
+    int i;
+    double total = 0.0;
+    int minutes = 0;
+    double secondes = 0.0;
+
+for(i=0; i<3; i++)
+{
+    double temps = 30.0 + (rand() / (double)RAND_MAX) * (50.0 - 40.0);
+    printf("Secteur %d : %.3f secondes\n", i+1 , temps);
+
+    total += temps;
+
+}
+
+    minutes = total/60;
+    secondes = fmod(total, 60);
+    //Afficher du temps total pour un tour (lap)
+    printf("Temps du tour (lap) : %.0d:%.3f secondes\n", minutes, secondes);
+}
