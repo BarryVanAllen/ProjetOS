c6737ae1d15374127d991d7a6f54f49f1a9854e2
BarryVanAllen <theo6740@hotmail.fr>
2024-10-24 11:49:13 +0200

    Update config.h

--- config.h
+++ config.h
@@ -1,12 +1,5 @@
 #ifndef CONFIG_H_INCLUDED
 #define CONFIG_H_INCLUDED
-#include <stdbool.h>
 
-typedef struct circuits circuits;
-struct circuits
-{
-    char nom[100];
-    bool sprint;
-};
 
 #endif // CONFIG_H_INCLUDED
