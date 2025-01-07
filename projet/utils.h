20f9a8834ad933b007ad2a2fc99549d2a2f94517
TheReturnedABought <145712267+TheReturnedABought@users.noreply.github.com>
2025-01-07 06:44:00 -0800

    Update utils.h

--- projet/utils.h
+++ projet/utils.h
@@ -14,4 +14,12 @@ typedef struct {
 float generer_temps_tour(float base_temps, int difficulte);
 void tri_pilotes(Pilote pilotes[], int nb_pilotes);
 
+const int NB_KM = 250;
+
+//Calcule le nombre de tour d'une course
+int calculNbTour(int tailleCircuit);
+
+//Calcule le nombre de tour de la course du dimanche
+int calculNbTourSprint(int tailleCircuit);
+
 #endif
