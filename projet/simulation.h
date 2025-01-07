111d82cb0f0dae9976ae6c0d80ba3b1a33ff88fd
TheReturnedABought <145712267+TheReturnedABought@users.noreply.github.com>
2025-01-07 07:05:14 -0800

    Update simulation.h

--- projet/simulation.h
+++ projet/simulation.h
@@ -1,6 +1,6 @@
 #ifndef SIMULATION_H
 #define SIMULATION_H
 
-void simulate_session(MemoirePartagee *mp, const char *session, int nb_tours, int nb_pilotes);
+void simulate_session( const char *session, int nb_tours, int nb_pilotes);
 
 #endif
