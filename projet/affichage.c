f93687cedd83227c8ff69c39c6cfb9766e7b5149
BarryVanAllen <theo6740@hotmail.fr>
2025-01-10 13:58:15 -0800

    Update affichage.c

--- projet/affichage.c
+++ projet/affichage.c
@@ -2,15 +2,6 @@
 #include <math.h>
 #include "types.h"
 
-void afficher_resultats_en_temps_reel(Pilote pilotes[], int tour, const char *session) {
-    printf("\033[H\033[J");
-    printf("--- Session : %s | Tour : %d ---\n", session, tour);
-    printf("Classement actuel :\n");
-    for (int i = 0; i < 20; i++) {
-        printf("%d. Pilote: %s | Meilleur temps: %.3f sec | Dernier tour: %.3f sec\n",
-               i + 1, pilotes[i].nom, pilotes[i].temps_meilleur_tour, pilotes[i].dernier_temps_tour);
-    }
-}
 
 void afficher_resultats(Pilote pilotes[], int nb_pilotes, const char *phase) {
     printf("\033[H\033[J"); // Nettoie l'écran (optionnel)
