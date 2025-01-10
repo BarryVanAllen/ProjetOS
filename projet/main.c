29bbb3ff1565fce73dd0281e2799aaadd0ce6d1b
TheReturnedABought <145712267+TheReturnedABought@users.noreply.github.com>
2025-01-10 15:05:15 +0100

    Update main.c

--- projet/main.c
+++ projet/main.c
@@ -45,17 +45,6 @@ void tri_pilotes(Pilote pilotes[], int nb_pilotes) {
     }
 }
 
-// Fonction pour afficher les résultats en temps réel
-void afficher_resultats_en_temps_reel(Pilote pilotes[], int tour, const char *session) {
-    printf("\033[H\033[J");
-    printf("--- Session : %s | Tour : %d ---\n", session, tour);
-    printf("Classement actuel :\n");
-    for (int i = 0; i < NB_PILOTES; i++) {
-        printf("%d. Pilote: %s | Meilleur temps: %.3f sec | Dernier tour: %.3f sec\n",
-               i + 1, pilotes[i].nom, pilotes[i].temps_meilleur_tour, pilotes[i].dernier_temps_tour);
-    }
-}
-
 // Executer phase de qualification
 void executer_phase_qualification(MemoirePartagee *mp, int nb_pilotes, const char *phase) {
     for (int tour = 1; tour <= NB_TOURS_QUALIF; tour++) {
