93beb2760f97fe183d366b55a28332452e9f676a
BarryVanAllen <theo6740@hotmail.fr>
2025-01-11 02:58:57 -0800

    Update affichage.h
    ajout de la fonction menu

--- projet/affichage.h
+++ projet/affichage.h
@@ -5,5 +5,7 @@
 void afficher_resultats_en_temps_reel(Pilote pilotes[], int tour, const char *session);
 void afficher_resultats(Pilote pilotes[], int nb_pilotes, const char *phase);
 void format_temps(float seconds, char *formatted_time);
+void menu(char *program_name);
+
 
 #endif
