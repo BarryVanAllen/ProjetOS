4052da43d5926dbb780f318b7cdbc48257a4689a
TheReturnedABought <145712267+TheReturnedABought@users.noreply.github.com>
2025-01-11 15:35:24 -0800

    Update sessions.h

--- projet/sessions.h
+++ projet/sessions.h
@@ -8,7 +8,7 @@ void traiter_session(const char *session, MemoirePartagee *mp);
 void afficher_eliminés(MemoirePartagee *mp, int start, int end);
 void afficher_top_10(MemoirePartagee *mp, int top);
 void executer_toutes_les_sessions(MemoirePartagee *mp);
-void executer_tour(MemoirePartagee *mp, int nb_pilotes, const char *phase, int nb_tours);
+void executer_tour(MemoirePartagee *mp, int nb_pilotes, char *phase, int nb_tours);
 void free_practice(MemoirePartagee *mp, int repeat);
 void qualification(MemoirePartagee *mp);
 void course(MemoirePartagee *mp);
