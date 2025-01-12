8d841947dcca43fab86d4765be15b3f913620d92
TheReturnedABought <145712267+TheReturnedABought@users.noreply.github.com>
2025-01-12 11:48:27 -0800

    Update file_manager.c

--- projet/file_manager.c
+++ projet/file_manager.c
@@ -35,7 +35,7 @@ void fin_gestion_semaphore(MemoirePartagee *mp, int is_writer) {
 
 void save_eliminated_cars(char filetosave, MemoirePartagee *mp){
 
-    FILE *file = fopen(file_to_save, "w");
+    FILE *file = fopen(filetosave, "w");
 
     if (file == NULL)
         perror("fopen failed !"), exit(EXIT_FAILURE);
