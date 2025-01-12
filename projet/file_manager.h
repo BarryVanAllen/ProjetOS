e88fc374d2d11bf290621f305054a41a2f3c54ce
TheReturnedABought <145712267+TheReturnedABought@users.noreply.github.com>
2025-01-12 11:31:18 -0800

    Update file_manager.h

--- projet/file_manager.h
+++ projet/file_manager.h
@@ -18,15 +18,15 @@ void fin_gestion_semaphore(MemoirePartagee *mp, int is_writer);
 *@param Pilote array[] le tableau qui contient les pilotes éliminés.
 */
 
-void save_eliminated_cars(charfiletosave, Pilote array[]);
+void save_eliminated_cars(charfiletosave, MemoirePartagee *mp);
 
 /** la fonction read_eliminated_cars lit les pilotes élimninés depuis un fichier
  *  vers un tableau qui va contenir le classement 
  * @param char filetoread
-*@param Pilote array[] le tableau qui contient le classmeent des Qualifs
+*@param int array[] le tableau qui contient le classmeent des Qualifs
 */
 
-void read_eliminated_cars(char filetoread, Pilote array[]); 
+void read_eliminated_cars(char filetoread, int array[]); 
 
 /**
  * Parses all rows from a CSV file into an array of Pilote structs.
